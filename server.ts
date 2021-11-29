40d1e6a280aeb4d94ee2c2e74e10b5cfb86e6294
Daniel Flanagan <daniel@lyte.dev>
2021-11-29 15:35:12 -0600

    Remove duplicate log

--- server.ts
+++ server.ts
@@ -6,7 +6,6 @@ export const server = async (handler: Handler) => {
   const hostname = await queryEnv("BIND_HOST", "localhost");
   const port: number = parseInt(await queryEnv("BIND_PORT", "0"));
   const listener: Deno.Listener = Deno.listen({ hostname, port });
-  console.log(listener, JSON.stringify(listener.addr));
   console.log(`Listening on port ${(listener.addr as Deno.NetAddr).port}`);
 
   for await (const conn of listener) {
